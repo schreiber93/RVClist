38395527906d990e5c2af93d230caf6ffc8ebdfd
Chuck Konkol <ckonkol@gmail.com>
2017-04-08 10:59:19 -0500

    aaaa

--- script.js
+++ script.js
@@ -2,17 +2,34 @@
 //v 4.0 read cookie on load and display
 
 //v4.0 
-var count = 0;
+var counter = 0;
+
 window.onload = function() {
-     alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
-  populateshoppinglistonload();
-   displayShoppinglists();
+ alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
+ populateshoppinglistonload();
+  displayShoppinglists();
     clearFocus();
 };
 
 function about()
 {
      alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
+
+}
+
+onloads();
+
+function onloads()
+{
+   if (counter == 0)
+   {
+   counter = 1;
+  populateshoppinglistonload();
+   displayShoppinglists();
+    clearFocus();
+ //alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
+
+   }
 
 }
 //read cookie and return
